61c325fc5d1565bc57525d1146bbbd0b2dd072c8
hephaisto <hephaisto@users.noreply.github.com>
2018-04-26 19:41:06 +0200

    Conditionally include <memory> to fix get_pointer

--- libs/librepcb/python/geometry.cpp
+++ libs/librepcb/python/geometry.cpp
@@ -1,3 +1,8 @@
+#include <boost/config.hpp>
+#if BOOST_VERSION < 105300 || defined BOOST_NO_CXX11_SMART_PTR 
+#include <memory>
+#endif
+
 #include <boost/python.hpp>
 
 #include "../common/units/length.h"
