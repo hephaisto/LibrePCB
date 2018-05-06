589df9891ae3d002d8c0f210d2e634ba61036423
hephaisto <hephaisto@users.noreply.github.com>
2018-05-06 18:44:19 +0200

    Default values for testing workspace

--- tests/python/common.py
+++ tests/python/common.py
@@ -6,6 +6,13 @@ def getWorkspaceLocation():
         wp = os.environ["TEST_WORKSPACE_LOCATION"]
         return lp.FilePath(wp)
     except KeyError:
-        return lp.determineWorkspacePath()
+        print("TEST_WORKSPACE_LOCATION not defined")
+        wp = lp.determineWorkspacePath()
+        if len(wp.toStr()) > 0:
+            return wp
+        else:
+            default_wp = "./dev/demo-workspace"
+            print("default workspace empty, defaulting to \"{}\"".format(default_wp))
+            return lp.FilePath(wp)
 
 print("using workspace location: \"{}\"".format(getWorkspaceLocation().toStr()))
